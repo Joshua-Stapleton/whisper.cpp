5e7b257473614a7a8ec149619082b30484fb6b09
Joshua-Stapleton <joshua.stapleton.ai@gmail.com>
2023-07-28 23:06:53 +0200

    added async email sending to function, next we have to add transcription

--- tools/gpt_functions.py
+++ tools/gpt_functions.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 import os
 import aiohttp
 import re
+import asyncio
 import json
+from tools.notification_functions import send_email, send_email_async
 load_dotenv()
 OPENAI_KEY = os.environ.get('OPEN_AI_KEY')
 
@@ -46,14 +48,16 @@ async def generate_gpt4_response_async(prompt:str, task_num:int) -> str:
     print("Generating GPT-4 response for task #", task_num, "...")
     openai.api_key = OPENAI_KEY
     messages = {'model': 'gpt-4', 'messages': [{'role': 'user', 'content': prompt}], 'temperature': 0.8}
-    api_endpoint = "https://api.openai.com/v1/chat/completions" # api_endpoint_from_url()
+    api_endpoint = "https://api.openai.com/v1/chat/completions"
     request_header = {"Authorization": f"Bearer {OPENAI_KEY}"}
     try:
         async with aiohttp.ClientSession() as session:
             async with session.post(url=api_endpoint, headers=request_header, json=messages) as response:
                 response = await response.json()
-                print("FINISHED TASK NUM", task_num, "RESPONSE", response['choices'][0]['message']['content'])
-                return response['choices'][0]['message']['content']
+                print("FINISHED TASK NUM", task_num, ". RESPONSE:", response['choices'][0]['message']['content'])
+                response = response['choices'][0]['message']['content']
+                asyncio.create_task(send_email_async(os.environ.get('SENDING_EMAIL_ADDRESS'), 'joshua.stapleton.ai@gmail.com', "FRED response for task " + str(task_num), response + "\n\n-----------------------\n\nTRANSCRIPT:\n", os.environ.get('EMAIL_PASSWORD')))
+                return response
 
     except Exception as e:
         print(f"Request {task_num} failed with Exception {e}")
