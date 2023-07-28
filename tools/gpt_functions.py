3377c4326d83d1dab336c8f60b7bfbc3078fe079
Joshua-Stapleton <joshua.stapleton.ai@gmail.com>
2023-07-28 22:47:47 +0200

    added async version of get_gpt4_response function

--- tools/gpt_functions.py
+++ tools/gpt_functions.py
@@ -1,8 +1,17 @@
 import openai
 from dotenv import load_dotenv
-import os 
+import os
+import aiohttp
+import re
+import json
 load_dotenv()
 OPENAI_KEY = os.environ.get('OPEN_AI_KEY')
+
+
+def api_endpoint_from_url(request_url):
+    """Extract the API endpoint from the request URL."""
+    match = re.search('^https://[^/]+/v\\d+/(.+)$', request_url)
+    return match[1]
 
 
 def generate_gpt3_response(prompt:str) -> str:
@@ -31,3 +40,20 @@ def generate_gpt4_response(prompt:str) -> str:
     )
 
     return response['choices'][0]['message']['content']
+
+
+async def generate_gpt4_response_async(prompt:str, task_num:int) -> str:
+    print("Generating GPT-4 response for task #", task_num, "...")
+    openai.api_key = OPENAI_KEY
+    messages = {'model': 'gpt-4', 'messages': [{'role': 'user', 'content': prompt}], 'temperature': 0.8}
+    api_endpoint = "https://api.openai.com/v1/chat/completions" # api_endpoint_from_url()
+    request_header = {"Authorization": f"Bearer {OPENAI_KEY}"}
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url=api_endpoint, headers=request_header, json=messages) as response:
+                response = await response.json()
+                print("FINISHED TASK NUM", task_num, "RESPONSE", response['choices'][0]['message']['content'])
+                return response['choices'][0]['message']['content']
+
+    except Exception as e:
+        print(f"Request {task_num} failed with Exception {e}")
